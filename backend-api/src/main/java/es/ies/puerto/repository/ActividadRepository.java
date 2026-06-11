dbce2cb287c9727bc64a705af281a143e77fb791
IvnMD <ivan.mesa.dominguez@gmail.com>
2026-06-11 23:40:09 +0100

    bugfix/correcciones backend

--- backend-api/src/main/java/es/ies/puerto/repository/ActividadRepository.java
+++ backend-api/src/main/java/es/ies/puerto/repository/ActividadRepository.java
@@ -1,8 +1,11 @@
 package es.ies.puerto.repository;
 
-import es.ies.puerto.model.Actividad;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import es.ies.puerto.model.Actividad;
+
 @Repository
-public interface ActividadRepository extends JpaRepository<Actividad, Long> {}
+public interface ActividadRepository extends JpaRepository<Actividad, Long> {
+    Actividad findActividadById(Long id);
+}
--- backend-api/src/main/java/es/ies/puerto/repository/UsuarioRepository.java
+++ backend-api/src/main/java/es/ies/puerto/repository/UsuarioRepository.java
@@ -1,8 +1,11 @@
 package es.ies.puerto.repository;
 
-import es.ies.puerto.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import es.ies.puerto.model.Usuario;
+
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {}
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Usuario findUsuarioById(Long id);
+}
